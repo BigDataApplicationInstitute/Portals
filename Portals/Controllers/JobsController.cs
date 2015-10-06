2959241bd1ea9204600e59f038d07a293be9bb28
BigDataApplicationInstitute <Shiyin@pc.ecdc.edgecast.com>
2015-10-06 00:58:45 -0700

    Init push

---
+++ Portals/Controllers/JobsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portals.Controllers
+{
+    public class JobsController : Controller
+    {
+        // GET: Job
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
