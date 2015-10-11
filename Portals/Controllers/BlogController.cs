d87d4667065f09634acb2c3e4bbd8518ff7becce
Shiyin <bigdatashiyin@gmail.com>
2015-10-11 16:47:11 -0700

    Added blog function

---
+++ Portals/Controllers/BlogController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Portals.Models;
+using Microsoft.AspNet.Identity;
+
+namespace Portals.Controllers
+{
+    public class BlogController : Controller
+    {
+        private CoursesEntities db = new CoursesEntities();
+        // GET: /Blog/
+        public ActionResult Index()
+        {
+            return View(db.Blog.ToList());
+        }
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Blog blogItem = db.Blog.Where(x => x.Id == id && x.Status == 1).FirstOrDefault();
+            if (blogItem == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.username = User.Identity.Name;
+            return View(blogItem);
+        }
+        [Authorize(Roles = "admin")]
+        public ActionResult AdminIndex()
+        {
+            return View(db.Blog.ToList());
+        }
+
+        // GET: /Blog/Create
+        [Authorize(Roles = "admin")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Blog/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost,ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                blog.PostTime = DateTime.Now;
+                blog.PostUserId = User.Identity.GetUserId();
+                blog.IsHome = 0;
+                blog.Status = 1;
+                db.Blog.Add(blog);
+                db.SaveChanges();
+                return RedirectToAction("AdminIndex");
+            }
+
+            return View(blog);
+        }
+
+        // GET: /Blog/Edit/5
+        [Authorize(Roles = "admin")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Blog blog = db.Blog.Find(id);
+            if (blog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blog);
+        }
+
+        // POST: /Blog/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost, ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(blog).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("AdminIndex");
+            }
+            return View(blog);
+        }
+
+        // GET: /Blog/Delete/5
+        [Authorize(Roles = "admin")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Blog blog = db.Blog.Find(id);
+            if (blog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blog);
+        }
+
+        // POST: /Blog/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Blog blog = db.Blog.Find(id);
+            db.Blog.Remove(blog);
+            db.SaveChanges();
+            return RedirectToAction("AdminIndex");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Portals/infrastructure/GeneralHelper.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portals.infrastructure
+{
+    public class GeneralHelper
+    {
+        // Summary:
+        // 顯示時間
+        public static string TimeAgo(DateTime d)
+        {
+
+            if (d == null)
+            {
+                return null;
+            }
+            TimeSpan s = DateTime.Now.Subtract(d);
+
+            // 2.
+            // Get total number of days elapsed.
+            int dayDiff = (int)s.TotalDays;
+
+            // 3.
+            // Get total number of seconds elapsed.
+            int secDiff = (int)s.TotalSeconds;
+
+            // 4.
+            // Don't allow out of range values.
+            if (dayDiff < 0)
+            {
+                return null;
+            }
+            else if (dayDiff > 30)
+            {
+                //return string.Format("{0} 個月前", Math.Ceiling((double)dayDiff / 30) - 1);
+                return d.ToShortDateString();
+            }
+            // 5.
+            // Handle same-day times.
+            if (dayDiff == 0)
+            {
+                // A.
+                // Less than one minute ago.
+                if (secDiff < 60)
+                {
+                    return "剛剛";
+                }
+                // B.
+                // Less than 2 minutes ago.
+                if (secDiff < 120)
+                {
+                    return "1 分鐘前";
+                }
+                // C.
+                // Less than one hour ago.
+                if (secDiff < 3600)
+                {
+                    return string.Format("{0} 分鐘前",
+                        Math.Floor((double)secDiff / 60));
+                }
+                // D.
+                // Less than 2 hours ago.
+                if (secDiff < 7200)
+                {
+                    return "1 小時前";
+                }
+                // E.
+                // Less than one day ago.
+                if (secDiff < 86400)
+                {
+                    return string.Format("{0} 小時前",
+                        Math.Floor((double)secDiff / 3600));
+                }
+            }
+            // 6.
+            // Handle previous days.
+            if (dayDiff == 1)
+            {
+                return "昨天";
+            }
+            if (dayDiff < 7)
+            {
+                return string.Format("{0} 天前",
+                dayDiff);
+            }
+            if (dayDiff < 31)
+            {
+                return string.Format("{0} 週前",
+                Math.Ceiling((double)dayDiff / 7));
+            }
+            return null;
+        }
+	}
+}
